426e890e75717285d53fbb8485a8b2fcf5f41683
onass <blisskynet@gmail.com>
2022-01-24 20:27:12 +0700

    Added header home and foot dir

--- blisskynet/src/App.js
+++ blisskynet/src/App.js
@@ -1,10 +1,15 @@
+import Home from './component/Home';
+import Header from './component/Header'
+import Footer from './component/Footer'
 import React from 'react';
 
 
 function App() {
   return (
     <div className="App">
-     Hello
+     <Header />
+     <Home />
+     <Footer />
     </div>
   );
 }
