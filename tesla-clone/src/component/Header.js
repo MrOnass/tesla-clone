b2d5b99240c59e7b129cab297afad0be184a2c28
onass <blisskynet@gmail.com>
2022-01-26 02:32:06 +0700

    Added slide menu

--- tesla-clone/src/component/Header.js
+++ tesla-clone/src/component/Header.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
+import MenuIcon from '@mui/icons-material/Menu';
+import CancelIcon from '@mui/icons-material/Cancel';
+
 
 function Header() {
+
+    const [burgerStatus,setBurgerStatus] = useState(false);
+
   return (
     <Container>
       <a href="#h">
@@ -16,7 +22,24 @@ function Header() {
       <RightMenu>
       <a href="#m">Shop</a>
       <a href="#m">Tesla Account</a>
+      <CustomMenu onClick={()=>setBurgerStatus(true)} />
       </RightMenu>
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+        <CustomClose onClick={()=>setBurgerStatus(false)} />
+        </CloseWrapper>
+      <li><a href="#m">Exisiting inventory</a></li>
+      <li><a href="#m">Used inventory</a></li>
+      <li><a href="#m">Trading </a></li>
+      <li><a href="#m">Cyber Truck</a></li>
+      <li><a href="#m">Roadster </a></li>
+      <li><a href="#m">Semi </a></li>
+      <li><a href="#m">Charging </a></li>
+      <li><a href="#m">Power </a></li>
+      <li><a href="#m">Trading </a></li>
+      <li><a href="#m">Utilities</a></li>
+      <li><a href="#m">Test Drive </a></li>
+      </BurgerNav>
     </Container>
   );
 }
@@ -25,11 +48,13 @@ min-height :60px;
 position: fixed;
 display: flex;
 align-items: center;
+justify-content: space-between;
 padding: 0 20px
 top:0;
 left: 0;
 right:0;
-`;
+z-index:1;
+`
 
 const Menu = styled.div`
   display: flex;
@@ -42,15 +67,53 @@ const Menu = styled.div`
     padding: 0 10px;
     flex-wrap: nowrap;
   }
-`;
+  @media(max-width: 768px){
+    display: none;
+  }
+`
 
 const RightMenu = styled.div`
+display: flex;
+align-items: center;
 a {
     font-weight: 600;
     text-transform: uppercase;
     margin-right: 10px;
-    flex-wrap: nowrap;
+    
   }
-`;
+`
+const CustomMenu = styled(MenuIcon )`
+cursor: pointer;
+`
+const BurgerNav =styled.div`
+position: fixed;
+top: 0;
+bottom: 0;
+Right: 0;
+background: white;
+width: 300px;
+z-index: 16;
+list-style: none;
+padding: 20px;
+display: flex;
+flex-direction: column;
+text-align: start;
+transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+transition: transform 0.2s;
+li{
+  padding: 15px 0;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  a{
+    font-weight: 600;
+  }
+}
+`
+const CustomClose = styled(CancelIcon)`
+cursor: pointer;
+`
+const CloseWrapper = styled.div`
+display: flex;
+justify-content: flex-end;
+`
 
 export default Header;
